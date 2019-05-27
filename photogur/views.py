492b8c7ae71641385cdee22e52c019a71b88770d
Jasmin Saromo <jasminsaromo@gmail.com>
2019-05-27 14:18:35 -0400

    created pictures_show template

--- photogur/views.py
+++ photogur/views.py
@@ -2,7 +2,16 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from photogur.models import Picture, Comment
 
+def root_path(request):
+    return HttpResponseRedirect('pictures/')
+
 def pictures(request):
     context = {'collection':Picture.objects.all()}
     response = render(request, 'pictures.html', context)
+    return HttpResponse(response)
+
+def picture_show(request, id):
+    picture = Picture.objects.get(pk=id)
+    context = {'picture': picture}
+    response = render(request, 'picture.html', context)
     return HttpResponse(response)
