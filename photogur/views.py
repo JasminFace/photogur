1306484e3d011fa1a4fd40d3383d31467b865a46
Jasmin Saromo <jasminsaromo@gmail.com>
2019-05-27 15:54:03 -0400

    created base.html file

--- photogur/views.py
+++ photogur/views.py
@@ -6,12 +6,18 @@ def root_path(request):
     return HttpResponseRedirect('pictures/')
 
 def pictures(request):
-    context = {'collection':Picture.objects.all()}
+    context = {
+        'title': 'Home',
+        'collection':Picture.objects.all()
+        }
     response = render(request, 'pictures.html', context)
     return HttpResponse(response)
 
 def picture_show(request, id):
     picture = Picture.objects.get(pk=id)
-    context = {'picture': picture}
+    context = {
+        'title': Picture.title,
+        'picture': picture
+        }
     response = render(request, 'picture.html', context)
     return HttpResponse(response)
